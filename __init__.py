b4ecc45197af715ad705f5ae6e35b7146f868d4d
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2020-06-02 22:54:46 +0300

    full_pipeline.py TODO! Sort not included so that lines are mixed. Tesseract barely integrated.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,34 @@
-from . import create_lmdb_dataset
-from . import dataset
-from . import demo
-from . import model
-from . import modules
-from . import test
-from . import train
-from . import utils
+try:
+    import recognition
+    import create_lmdb_dataset
+    import dataset
+    import demo
+    import model
+    import modules
+    import test
+    import train
+    import utils
+except:
+    try:
+        import minimal_text_recognition.recognition
+        import minimal_text_recognition.create_lmdb_dataset
+        import minimal_text_recognition.dataset
+        import minimal_text_recognition.demo
+        import minimal_text_recognition.model
+        import minimal_text_recognition.modules
+        import minimal_text_recognition.test
+        import minimal_text_recognition.train
+        import minimal_text_recognition.utils
+    except:
+        import recognition.minimal_text_recognition.recognition
+        import recognition.minimal_text_recognition.create_lmdb_dataset
+        import recognition.minimal_text_recognition.dataset
+        import recognition.minimal_text_recognition.demo
+        import recognition.minimal_text_recognition.model
+        import recognition.minimal_text_recognition.modules
+        import recognition.minimal_text_recognition.test
+        import recognition.minimal_text_recognition.train
+        import recognition.minimal_text_recognition.utils
 
 # from recognition.minimal_text_recognition import create_lmdb_dataset
 # from recognition.minimal_text_recognition import dataset
